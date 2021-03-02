3171b9d2abb4d68d8db3bceba98038ec908eb721
Will B <willhbjr@gmail.com>
2021-03-02 05:19:07 +0000

    added for in statements

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+let astronautID;
+
 let superChimpOne = {
     name: "Chad",
     species: "Chimpanzee",
     mass: 9,
     age: 6
+   // astronautID: = Math.floor(Math.random()*10),
 };
 
 let salamander = {
@@ -33,7 +36,36 @@ let superInsect = {
     age: 5
 };
 
+superInsect[astronautID] = Math.floor(Math.random()*10);
+superChimpOne[astronautID] = Math.floor(Math.random()*10);
+superChimpTwo[astronautID] = Math.floor(Math.random()*10);
+superCanine[astronautID] = Math.floor(Math.random()*10);
+salamander[astronautID] = Math.floor(Math.random()*10);
 
+
+for (item in superChimpOne){
+  console.log(item + ", " + superChimpOne[item]);
+}
+
+console.log("\n");
+for (item in superChimpTwo){
+  console.log(item + ", " + superChimpTwo[item]);
+}
+
+console.log('\n');
+for (item in superInsect){
+  console.log(item + ", " + superInsect[item]);
+}
+
+console.log('\n');
+for (item in salamander){
+  console.log(item + ", " + salamander[item]);
+}
+
+console.log('\n');
+for (item in superCanine){
+  console.log(item + ", " + superCanine[item]);
+}
 // After you have created the other object literals, add the astronautID property to each one.
 
 // Create an array to hold the animal objects.
